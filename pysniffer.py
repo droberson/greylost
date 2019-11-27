713cc7fb238bf2cf9f993593de34030f53b7d3cc
Daniel Roberson <daniel@planethacker.net>
2019-11-27 09:47:17 -0800

    initial commit

---
+++ pysniffer.py
@@ -0,0 +1,58 @@
+""" pysniffer - libpcap-based sniffer. """
+
+# pylint: disable=I1101
+import pcapy
+
+from pypacket import Packet
+
+
+class Sniffer():
+    """ Sniffer() - Sniffer object.
+
+    Attributes:
+        interface (string) - Interface to sniff on.
+        promisc (int)      - Promiscuous mode; 1 = yes, 0 = no
+        bpf (str)          - BPF filter for sniffer.
+        snaplen (int)      - snaplen
+        timeout (int)      - timeout.
+        sniffer            - fd used by libpcap.
+    """
+    def __init__(self, interface, promisc=1, bpf="", snaplen=65535, timeout=100):
+        self.interface = interface
+        self.promisc = promisc
+        self.bpf = bpf
+        self.snaplen = snaplen
+        self.timeout = timeout
+        self.sniffer = None
+
+    def start(self):
+        """ Sniffer.start() - Start the sniffer.
+
+        Args:
+            None.
+
+        Returns:
+            Nothing.
+        """
+        self.sniffer = pcapy.open_live(self.interface,
+                                       self.snaplen,
+                                       self.promisc,
+                                       self.timeout)
+        self.sniffer.setfilter(self.bpf)
+
+    def next(self):
+        """ Sniffer.next() - Get next packet from sniffer.
+
+        Args:
+            None.
+
+        Returns:
+            Packet object of the next packet on success, None on failure.
+        """
+        _, packet = self.sniffer.next()
+        return Packet(packet) if packet else None
+
+    def setnonblock(self):
+        """ Sniffer.setnonblock() - set sniffer to non-blocking mode. """
+        # TODO error check
+        self.sniffer.setnonblock(1)
